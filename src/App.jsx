330938447ca28fc7dc61c8553591fff8e0184f91
charlie070102 <charlie070102@hotmail.com>
2024-03-27 10:43:51 -0500

    Mensaje descriptivo de los cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
 
-
 function App() {
   return (
-      <div className="App">
-          <Navbar/>
-          <Footer/>
-      </div>
+    <div className="App">
+      <Navbar />
+      <Footer />
+    </div>
   );
 }
 
 export default App;
+
+//este texto es para identificar que esta es la ubicacion y el proyecto correcto a subir de carlos clavijo prueba repo pus
